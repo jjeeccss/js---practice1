b821176ed576427783dc07d893aa353726067c3f
jjeeccss <jeicogacusana24@gmail.com>
2024-09-14 14:53:09 +0800

    Delete variables.js

--- variables.js
+++
@@ -1,8 +0,0 @@
-let name ="Jecs";
-let whatDoYoWannaBecomeInYourLife = "A programmer";
-let gender = "Male";
-let twitterHandle = "none";
-console.log(name);
-console.log(whatDoYoWannaBecomeInYourLife);
-console.log(gender);
-console.log(twitterHandle);
